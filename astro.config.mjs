e2c5b8310ee50c21d34eac2de782ee3b2a65b06b
Alese11 <oluaese@gmail.com>
2024-05-20 01:58:43 +0200

    Again

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+import { defineConfig } from "astro/config";
+ import icon from "astro-icon";
+
+export default defineConfig({
+  integrations: [icon()],
+  styles: [
+    'bootstrap/dist/css/bootstrap.min.css'
+  ],
+});
